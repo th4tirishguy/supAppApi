6c6c6213b33e8de1e9a64432108cfad22df9197e
Mark McCormack <mark@MacbookPro.local>
2016-06-10 16:33:19 +0100

    friendships not fully implemented

--- models/user.js
+++ models/user.js
@@ -14,7 +14,34 @@ var userSchema = new Schema({
 userSchema.methods.addFriend = function(friend, cb) {
 	//this.friends.push(friend);
 	//console.log(Object.getOwnPropertyNames(this.model('User')));
-	console.log(friend);
+	console.log(Object.getOwnPropertyNames(this.friends));
+	
+	// if (this.friends.indexOf(friend._id) > -1) {
+	// 	return console.log('Already a friend');
+	// }
+	// else {
+	// 	return console.log('Not a friend yet');
+	// }
+
+	if (friend) {
+		var current = this;
+		this.model('User').findOne({name: friend.name}, function(err, f) {
+			if (err) {
+				return console.log(err);
+			}
+			else {
+				if (current.friends.indexOf(f._id) > -1) {
+					return console.log('Already a friend');
+				}
+				else {
+					return console.log('Not a friend yet');
+				}
+			}
+		});
+	}
+	else {
+		return console.log('Not a valid name');
+	}
 	//this.save(cb);
 }
 
